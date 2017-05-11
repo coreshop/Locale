0195ba6fcce7288bfe42f18c2dec919fa8472ff0
dpfaffenbauer <dominik@lineofcode.at>
2017-05-11 18:25:31 +0200

    [Refactor] move all to src/CoreShop folder

---
+++ Context/PimcoreLocaleContext.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * CoreShop.
+ *
+ * This source file is subject to the GNU General Public License version 3 (GPLv3)
+ * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
+ * files that are distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2015-2017 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
+ * @license    https://www.coreshop.org/license     GNU General Public License version 3 (GPLv3)
+*/
+
+namespace CoreShop\Component\Locale\Context;
+
+use Pimcore\Service\Locale;
+
+class PimcoreLocaleContext implements LocaleContextInterface
+{
+    /**
+     * @var Locale
+     */
+    private $pimcoreLocaleService;
+
+    /**
+     * @param Locale $pimcoreLocaleService
+     */
+    public function __construct(Locale $pimcoreLocaleService)
+    {
+        $this->pimcoreLocaleService = $pimcoreLocaleService;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getLocaleCode()
+    {
+        return $this->pimcoreLocaleService->getLocale();
+    }
+}
